21deffbdaeabd527e24182f8c96c1be3b02ca63d
Arno Blümel <arno.bluemel@gmail.com>
2019-10-20 11:11:59 +0200

    step 17 - OnClickListener

--- DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package at.htl.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById<Button>(R.id.bt_roll)
-        rollButton.text = "Let's ROLL!!!"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
