59632e1dfce1691120d81efb72f2d576843366e1
Arno Blümel <arno.bluemel@gmail.com>
2019-10-20 11:08:31 +0200

    step 16 - findViewById

--- DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
@@ -2,11 +2,14 @@ package at.htl.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById<Button>(R.id.bt_roll)
+        rollButton.text = "Let's ROLL!!!"
     }
 }
